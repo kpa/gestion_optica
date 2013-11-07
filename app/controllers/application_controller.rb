460f696ef2b7ed79478001940a40d1fc9df6b38a
Roberto Romero <roberto@inaka.net>
2013-11-07 12:02:24 -0300

    Fix application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 # -*- encoding : utf-8 -*-
 class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+  before_filter :authenticate
   protect_from_forgery
+
+  def site_name
+    "Óptica"
+  end
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "kpa" && password == "arenakar"
+    end
+  end
 end
