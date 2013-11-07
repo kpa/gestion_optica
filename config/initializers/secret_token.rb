698d71c4ce791c0d4b46f57aa3ff6039ad27a310
Roberto Romero <roberto@inaka.net>
2013-11-07 10:51:12 -0300

    Restore secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-OpticaArena::Application.config.secret_token = '24a2ffcc437990d97d49492cf34998b06cb58595e8469c5eece3a20226743fd1860f470d9f3dfed4276d871eb9857373279239c2811f9f1cb66fafac9572dc7f'
+OpticaArena::Application.config.secret_token = '232bb6b9843fa2471157995d122cf3d1a201391f4267c1c8f204eee64527ef3c088ee4d449ad6c38c950747d5c0890d66055a1f76410751d5d673db652a137ef'
