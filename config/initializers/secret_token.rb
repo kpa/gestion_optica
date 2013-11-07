54e49a86236d9920e07b5e47f78115746e42244e
Roberto Romero <roberto@inaka.net>
2013-11-07 10:49:39 -0300

    Upgrading rails version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpticaArena::Application.config.secret_token = '24a2ffcc437990d97d49492cf34998b06cb58595e8469c5eece3a20226743fd1860f470d9f3dfed4276d871eb9857373279239c2811f9f1cb66fafac9572dc7f'
