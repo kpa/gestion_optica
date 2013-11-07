68174d6c2728d42b8c71508310dfe60be3ad2e3c
Roberto Romero <roberto@inaka.net>
2013-11-07 11:33:03 -0300

    Fix class names

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-OpticaArena::Application.config.session_store :cookie_store, :key => '_optica.arena_session'
+GestionOpticaArena::Application.config.session_store :cookie_store, :key => '_optica.arena_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# OpticaArena::Application.config.session_store :active_record_store
+# GestionOpticaArena::Application.config.session_store :active_record_store
